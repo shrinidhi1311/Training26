7493aa7ab2894cc0952989c4da86c950204dd235
shrinidhi <shrinidhi.saravanaraj@trumpf.com>
2026-07-16 11:38:34 +0530

    T1:Balanced Brackets

--- Training26/Program.cs
+++ Training26/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class Solution {
+   public bool IsValid (string s) {
+      Stack<char> stack = new Stack<char> ();
+      Dictionary<char, char> bpair = new Dictionary<char, char> () {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+
+      foreach (char c in s) {
+         if (bpair.ContainsKey (c)) {
+            if (stack.Count == 0 || stack.Peek () != bpair[c]) {
+               return false;
+            }
+            stack.Pop ();
+         } else {
+            stack.Push (c);
+         }
+      }
+
+      return stack.Count == 0;
+   }
+}
